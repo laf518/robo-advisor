171923a88aa79bd785dec8fb4f29ffabef4574b3
laf518 <laf518@stern.nyu.edu>
2020-06-15 11:20:11 -0400

    Update robo_advisor.py
    created user input for stock symbol with validation feature.

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,6 @@
 # app/robo_advisor.py
 import requests
+import json
 from dotenv import load_dotenv
 import pandas
 import os
@@ -9,10 +10,34 @@ import os
 
 load_dotenv()
 key = os.environ.get("ALPHAVANTAGE_API_KEY")
-breakpoint()
+# breakpoint()
+# COMPLETE
 
 ## Functionality requirements:
 # Information input / validation
+"""Create a while loop that sends user input to
+the API. If the symbol is invalid, an error should
+be returned by the API. Thus if error status = true,
+program will return an error message. Otherwise it 
+will break out of the WHILE loop.
+
+Use: TIME_SERIES_DAILY function"""
+while True:
+
+    quote = input("Please type in the stock symbol for which you are interested: ").upper()
+    request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + quote + "&apikey=" + key
+
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    
+    validation = list(parsed_response.keys())[0]
+
+    if validation != "Error Message":
+        break
+
+    else:
+        print("The stock symbol you submitted is invalid. Please try again.")
+breakpoint()
 # Information output
 
 ## Calculation requirements:
