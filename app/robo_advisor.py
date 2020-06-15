44f447ce41d2d13aec45eb9c2d0c58534c17b942
laf518 <laf518@stern.nyu.edu>
2020-06-15 10:06:44 -0400

    robo-advisor
    Initial setup

---
+++ app/robo_advisor.py
@@ -0,0 +1,34 @@
+# app/robo_advisor.py
+import requests
+import python-dotenv
+import pandas
+
+## Security requirements:
+# Create environmental variable for API Key
+
+
+## Functionality requirements:
+# Information input / validation
+# Information output
+
+## Calculation requirements:
+# List close, high and low prices
+
+## Starter code:
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: 2018-02-20")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
+
